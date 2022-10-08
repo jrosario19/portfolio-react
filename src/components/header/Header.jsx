650c72f8cdf71ce2b82fc2ad65e1fd860bf2db51
Juan Rosario <jrosario19@hotmail.com>
2022-10-08 19:08:42 -0400

    Add Header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/Foto-Perfil.jpg'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Juan Fco. Rosario Suli</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials/>
+
+        <div className="me">
+          <img src={ME} alt="me" className='profile__photo'/>
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
