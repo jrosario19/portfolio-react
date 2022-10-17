46723a7a3de4978aeb680808e439cc7fa90b0a43
Juan Rosario <jrosario19@hotmail.com>
2022-10-17 11:12:49 -0400

    Update portfolio section and readme file

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -14,36 +14,48 @@ const listOfWork = [{
   Imge: IMG6,
   Github: 'https://github.com/NeshGogo/Sistema-de-votacion',
   liveLink: 'Unavailable',
+  techs: "ASP.Net Core MVC",
+  desc:"This is a voting system that is based on the entire process that is carried out during the election of a candidate from a country. This system contains two aspects, one is the administrative part and the second part is the one used by the citizen who is going to vote."
 }, {
   Id: '2',
   Title: 'Book for movie and tv show',
   Imge: IMG1,
   Github: 'https://github.com/Trictonicmp/js-capstone-project',
   liveLink: 'https://trictonicmp.github.io/js-capstone-project/',
+  techs: "JavaScript",
+  desc:"Book tv app to keep track of tv shows, like a show and comment about them!"
 }, {
   Id: '3',
   Title: 'Budget Management System',
   Imge: IMG2,
   Github: 'https://github.com/jrosario19/rails-budget',
   liveLink: 'https://tranquil-mesa-74989.herokuapp.com/',
+  techs: "Ruby on Rails",
+  desc:"A Mobile Web App made with Rails that takes care of your expenses. You can have a record of your expenses by date, category, amount, and others"
 }, {
   Id: '4',
   Title: 'Vet Appointment Management System',
   Imge: IMG5,
   Github: 'https://github.com/rskinnerc/vet-appointments-rails',
   liveLink: 'https://vet-appointments.deply.cyou/',
+  techs: "Ruby on Rails",
+  desc:"This is an app in which you can reserve an appointment with a Veterinarian, create a Doctor, delete a Doctor and show all your appointments. This app was created separating the Fron-End and the Back-End in two repositories"
 }, {
   Id: '5',
   Title: 'Personal Portfolio',
   Imge: IMG4,
   Github: 'https://github.com/jrosario19/portfolio-react',
   liveLink: 'Unavailable',
+  techs: "React",
+  desc:"This is my personal Portfolio. Its purpose is for me to have a document in which I can show to recruiters and hiring managers my professional journey and demonstrate my skills and abilities."
 }, {
   Id: '6',
   Title: 'Math Maggians',
   Imge: IMG3,
   Github: 'https://github.com/jrosario19/math-magicians',
   liveLink: 'https://math-juan-rosario.herokuapp.com/',
+  techs: "React",
+  desc:"Math magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: Make simple calculations and Read a random math-related quote."
 }];
 
 
@@ -58,11 +70,13 @@ const Portfolio = () => {
        {
         listOfWork.map((item)=>{
           return (
-            <article key={item.Id} className='portfolio__item'>
+        <article key={item.Id} className='portfolio__item'>
           <div className="portfolio__item-image">
             <img src={item.Imge} alt={item.Title} />
           </div>
           <h3>{item.Title}</h3>
+          <p className='paragraph-desc'>{item.desc}</p>
+          <p className='project-tech'>{item.techs}</p>
           <div className="portfolio__item-cta">
             <a href={item.Github} className='btn' target='_blank'>Github</a>
             <a href={item.liveLink} className='btn btn-primary' target='_blank'>Live Demo</a>
